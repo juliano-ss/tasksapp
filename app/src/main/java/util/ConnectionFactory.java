fb1b33ff06d574c5b7a6accbf261f4fa147a1db5
juliano-ss <jotaliano@live.com>
2023-04-06 16:29:23 -0300

    primeiro commit

---
+++ app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+/**
+ *
+ * @author juliano
+ */
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/taskapp";
+    public static final String USER = "root";
+    public static final String PASS = "";       
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+
+            if (resultSet != null) {
+                connection.close();
+            }
+
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+
+        try {
+
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (resultSet != null) {
+                connection.close();
+            }
+
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conex?o com o banco de dados", ex);
+        }
+
+    }
+
+}
+
